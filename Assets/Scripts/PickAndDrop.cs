496ac67a7d969351d8360e7788b77dd434af4db9
Joshuaflores12 <joshuagabriellflores@gmail.com>
2024-02-11 14:11:51 -0500

    added notes

--- Assets/Scripts/PickAndDrop.cs
+++ Assets/Scripts/PickAndDrop.cs
@@ -11,7 +11,7 @@ public class PickAndDrop : MonoBehaviour
     [SerializeField] private float throwForce;
     private void Start()
     {
-        inputManager = InputManager.Instance;
+        inputManager = InputManager.Instance; // calls the input manager class singleton
     }
 
     void Update()
@@ -21,17 +21,17 @@ public class PickAndDrop : MonoBehaviour
 
     private void Grab()
     {
-        Ray ray = inputManager.GetCrosshairPoint();
+        Ray ray = inputManager.GetCrosshairPoint(); // adds raycast on the players cursor point
         RaycastHit hit;
         float maxDistance = 100f;
 
-        if (!inputManager.IsPlayerGrabbing())
+        if (!inputManager.IsPlayerGrabbing()) // if the player is not grabbing the object release the object
         {
             ReleaseObject();
             return;
         }
 
-        if (Physics.Raycast(ray, out hit)
+        if (Physics.Raycast(ray, out hit) // if the ray cast hit the tag item as "Object" calculates the grabbable distance rannge, upon hit it will be child to the object holder parent
             && hit.transform.CompareTag("Object")
             && Vector3.Distance(transform.position, hit.point) <= maxDistance
             && grabbedObject == null)
@@ -40,7 +40,7 @@ public class PickAndDrop : MonoBehaviour
             grabbedObject.parent = objectHolder;
         }
 
-        if (grabbedObject != null)
+        if (grabbedObject != null) // if the grabbed item is not null set it's target position to the object holder, It will move towards on the object holder position based on the amount of move speed it has
         {
             Vector3 targetPosition = objectHolder.position;
             grabbedObject.position = Vector3.MoveTowards(grabbedObject.position, targetPosition, objectMoveSpeed * Time.deltaTime);
@@ -51,7 +51,7 @@ public class PickAndDrop : MonoBehaviour
         }
     }
 
-    private void ReleaseObject()
+    private void ReleaseObject() //upon releasing the object the variables below will be null/empty
     {
         if (grabbedObject != null)
         {
@@ -60,7 +60,7 @@ public class PickAndDrop : MonoBehaviour
         }
     }
 
-    private void ThrowObject()
+    private void ThrowObject() // If the item is not null, throws the object forward upon reaching the object holder and the strength of the throw force will multiplied of the object holder direction, gets it's rigid body component and throw velocity of the rigid body
     {
         if (grabbedObject != null)
         {
